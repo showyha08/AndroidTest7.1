88ee2b4c796ac9ce0dfa16558782210a6b1bb517
showyha08 <over.and.uver@gmail.com>
2023-06-15 23:55:06 +0900

    done

--- app/src/main/java/com/websarva/wings/android/intentsample/MenuThanksActivity.kt
+++ app/src/main/java/com/websarva/wings/android/intentsample/MenuThanksActivity.kt
@@ -10,15 +10,16 @@ class MenuThanksActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_thanks)
 
-        //リスト画面から渡されたデータを取得
+        // リスト画面から渡されたデータを取得
+        // 受け取る場合はget型Extraとなる
         val menuName = intent.getStringExtra("menuName")
         val menuPrice = intent.getStringExtra("menuPrice")
 
-        //定食名と金額を表示させるTextViewを取得
+        // 定食名と金額を表示させるTextViewを取得
         val tvMenuName = findViewById<TextView>(R.id.tvMenuName)
         val tvMenuPrice = findViewById<TextView>(R.id.tvMenuPrice)
 
-        //TextViewに定食名と金額を表示
+        // TextViewに定食名と金額を表示
         tvMenuName.text = menuName
         tvMenuPrice.text = menuPrice
     }
